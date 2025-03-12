fb68c1c36c019f2bf653f64eb41a2cbaceea4327
JPChoyon <kotjpckhan@gmail.com>
2025-03-12 15:43:57 +0600

    user validation added

--- src/app/modules/orders/order.routes.ts
+++ src/app/modules/orders/order.routes.ts
@@ -2,15 +2,17 @@ import express from 'express';
 import { orderController } from './order.controller';
 import validator from '../../middlewares/validator';
 import { orderValidationSchema } from './order.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/order',
-  validator(orderValidationSchema),
+  validator(orderValidationSchema),auth(USER_ROLE.customer),
   orderController.createOrder,
 );
-router.get('/orders/:customerId', orderController.getCustomerOrders);
+router.get('/orders/:customerId',auth(USER_ROLE.customer), orderController.getCustomerOrders);
 router.put('/profile', (req, res) => {
   res.json({ message: 'Profile updated successfully (dummy implementation)' });
 });
